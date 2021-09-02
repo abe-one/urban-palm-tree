24c2d5ee8d7b6e5240b88ac510a4ead8d1a62d10
abe-one <77995299+abe-one@users.noreply.github.com>
2021-09-02 13:53:30 -0500

    Integrate Redux Store Into Lots Component

--- src/components/Lots.js
+++ src/components/Lots.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchLots, toggleSavedLots } from "../utils/reduxStore/actions";
 
-const Lots = () => {
-  return <div>Render array of Cards displaying lot data</div>;
+const Lots = ({ lots, fetchLots, toggleSavedLots }) => {
+  useEffect(() => {
+    fetchLots();
+  }, [fetchLots]);
+
+  return (
+    <div>
+      Render array of Cards displaying lot data
+      {lots.data.map((lot) => {
+        return <p key={lot.lotId}>{lot.description}</p>;
+      })}
+    </div>
+  );
 };
 
-export default Lots;
+const mapStateToProps = (state) => {
+  return { lots: state.lots };
+};
+
+export default connect(mapStateToProps, { fetchLots, toggleSavedLots })(Lots);
