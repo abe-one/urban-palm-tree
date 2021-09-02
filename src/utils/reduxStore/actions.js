f890c0b9055a072a68fbf702c1ed46274a4609c8
abe-one <77995299+abe-one@users.noreply.github.com>
2021-09-02 14:10:26 -0500

    Define All Actions

--- src/utils/reduxStore/actions.js
+++ src/utils/reduxStore/actions.js
@@ -1,9 +1,12 @@
-//\\\\\\\\\\\\\\\\\\\ ACTION TYPES \\\\\\\\\\\\\\\\\\\\\
-
 import { API } from "../MockAPI";
+
+//\\\\\\\\\\\\\\\\\\\ ACTION TYPES \\\\\\\\\\\\\\\\\\\\\
 
 export const FETCH_LOTS_SUCCESS = "FETCH_LOTS_SUCCESS";
 export const TOGGLE_SAVED_LOTS = "TOGGLE_SAVED_LOTS";
+export const FETCH_HOME_PLANS_SUCCESS = "FETCH_HOME_PLANS_SUCCESS";
+export const TOGGLE_SAVED_HOME_PLANS = "TOGGLE_SAVED_HOME_PLANS";
+export const FETCH_COMBINATIONS = "FETCH_COMBINATIONS";
 
 //\\\\\\\\\\\\\\\\\\\ ACTIONS \\\\\\\\\\\\\\\\\\\\\
 
@@ -14,4 +17,17 @@ export const fetchLots = () => {
 
 export const toggleSavedLots = () => {
   return { type: TOGGLE_SAVED_LOTS };
+};
+export const fetchHomePlans = () => {
+  const lots = API.getHomePlans();
+  return { type: FETCH_HOME_PLANS_SUCCESS, payload: lots };
+};
+
+export const toggleSavedHomePlans = () => {
+  return { type: TOGGLE_SAVED_HOME_PLANS };
+};
+
+export const fetchCombinations = () => {
+  const combos = API.getCombinations();
+  return { type: FETCH_COMBINATIONS, payload: combos };
 };
