7363c434cccc11c4d29b8dd1dd7fafc8b840f07d
abe-one <77995299+abe-one@users.noreply.github.com>
2021-09-03 00:28:05 -0500

    Refactor Lots Component

--- src/components/Lots.js
+++ src/components/Lots.js
@@ -1,51 +1,35 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { toggleSavedLots } from "../utils/reduxStore/actions";
+import React from "react";
 import InfoCard from "./InfoCard";
-const Lots = ({ lots, toggleSavedLots, mainGrid }) => {
-  const [displayAll, setDisplayAll] = useState(true);
 
+const Lots = ({ data, toggleSavedStatus, mainGrid, displayAll }) => {
   return (
     <>
-      <button onClick={() => setDisplayAll(!displayAll)}>
-        Show {displayAll ? "Saved" : "All"} Lots
-      </button>
-      <div
-        style={{
-          display: "flex",
-        }}
-      >
-        {lots.data.map(
-          ({ lotId, address, acres, description, image, saved }, index) => {
-            const detailsString = `${acres} - ${acres * 43_560} sqft`;
-            //TODO: Split address at comma
-            if (!displayAll && !saved) {
-              return null;
-            } else {
-              return (
-                <InfoCard
-                  key={lotId}
-                  stateKey="lots"
-                  headerImg={image}
-                  title={address}
-                  details={detailsString}
-                  description={description}
-                  savedStatus={saved}
-                  toggleSavedStatus={toggleSavedLots}
-                  index={index}
-                  gridCard={mainGrid}
-                />
-              );
-            }
+      {data.map(
+        ({ lotId, address, acres, description, image, saved }, index) => {
+          const detailsString = `${acres} - ${acres * 43_560} sqft`;
+          //TODO: Split address at comma
+          if (!displayAll && !saved) {
+            return null;
+          } else {
+            return (
+              <InfoCard
+                key={lotId}
+                stateKey="lots"
+                headerImg={image}
+                title={address}
+                details={detailsString}
+                description={description}
+                savedStatus={saved}
+                toggleSavedStatus={toggleSavedStatus}
+                index={index}
+                gridCard={mainGrid}
+              />
+            );
           }
-        )}
-      </div>
+        }
+      )}
     </>
   );
 };
 
-const mapStateToProps = (state) => {
-  return { lots: state.lots };
-};
-
-export default connect(mapStateToProps, { toggleSavedLots })(Lots);
+export default Lots;
